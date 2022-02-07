5e42359448054223b6e1e15a01b5df901bb86aee
Kalitha <kalithadissanayake44@gmail.com>
2022-02-07 21:25:59 +0530

    cal1

---
+++ src/cal.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+function Cal() {
+  return (
+    <div>
+      <div className="calculator-grid">
+      
+      <div className="output">
+
+
+        <div className="previous">0</div>
+        <div className="curent">0</div>
+
+      </div>
+
+      <button className="span-two">AC</button>
+      <button >DEL</button>
+      <button >/</button>
+      <br />
+      <button >1</button>
+      <button >2</button>
+      <button >3</button>
+      <button >*</button>
+      <br />
+      <button >4</button>
+      <button >5</button>
+      <button >6</button>
+      <button >+</button>
+      <br />
+      <button >7</button>
+      <button >8</button>
+      <button >9</button>
+      <button >-</button>
+      <br />
+      <button >.</button>
+      <button >0</button>
+      <button className="span-two">=</button>
+      
+     
+    </div>
+    </div>
+    
+  )
+}
+
+export default Cal;
