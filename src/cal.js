f69c408001daa5e75376526d065887b4aac2fbb0
Kalitha <kalithadissanayake44@gmail.com>
2022-02-07 22:17:29 +0530

    cal2

--- src/cal.js
+++ src/cal.js
@@ -1,45 +1,45 @@
 import React from 'react';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 function Cal() {
   return (
-    <div>
-      <div className="calculator-grid">
-      
-      <div className="output">
+
+      <div>
+
+        <div className="output">
 
 
-        <div className="previous">0</div>
-        <div className="curent">0</div>
+          <div className="previous">0</div>
+          <div className="curent">0</div>
 
+        </div>
+        <div >
+          <button className=" wide span-two btn btn-info">AC</button>
+          <button className=' btn btn-info' >DEL</button>
+          <button className=' btn btn-info'>/</button>
+          <br />
+          <button className=' btn btn-info'>1</button>
+          <button className=' btn btn-info'>2</button>
+          <button className=' btn btn-info'>3</button>
+          <button className=' btn btn-info'>*</button>
+          <br />
+          <button className=' btn btn-info'>4</button>
+          <button className=' btn btn-info'>5</button>
+          <button className=' btn btn-info'>6</button>
+          <button className=' btn btn-info'>+</button>
+          <br />
+          <button className=' btn btn-info'>7</button>
+          <button className=' btn btn-info'>8</button>
+          <button className=' btn btn-info'>9</button>
+          <button className=' btn btn-info'>-</button>
+          <br />
+          <button className=' btn btn-info'>.</button>
+          <button className=' btn btn-info'>0</button>
+          <button className="wide span-two btn btn-info">=</button>
+        </div>
       </div>
 
-      <button className="span-two">AC</button>
-      <button >DEL</button>
-      <button >/</button>
-      <br />
-      <button >1</button>
-      <button >2</button>
-      <button >3</button>
-      <button >*</button>
-      <br />
-      <button >4</button>
-      <button >5</button>
-      <button >6</button>
-      <button >+</button>
-      <br />
-      <button >7</button>
-      <button >8</button>
-      <button >9</button>
-      <button >-</button>
-      <br />
-      <button >.</button>
-      <button >0</button>
-      <button className="span-two">=</button>
-      
-     
-    </div>
-    </div>
-    
+
+
   )
 }
 
